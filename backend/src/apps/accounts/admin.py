0dc0307413ad76a3dd8c4ab00ca71df8fd077ac7
Yurii <yuriydorosh2005@gmail.com>
2024-04-27 23:00:12 +0300

    UPDATE: accounts/admin.py(added user table)

--- backend/src/apps/accounts/admin.py
+++ backend/src/apps/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "date_added")
+    list_filter = ("id", "username", "email")
+    search_fields = ("username", "email")
