5339ff04ff62b6b23212224f59a545157101a1be
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 00:26:42 +0300

    UPDATE: config/urls

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -1,22 +1,35 @@
-"""
-URL configuration for app project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Helmee API",
+        default_version="v1",
+        description="Documentation for helmee API",
+        contact=openapi.Contact(email="contact.yuriidorosh@gmail.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+API_PATH = "api/v1/"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{API_PATH}/users/', include('accounts.urls')),
+    
+    path(f'{API_PATH}/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),  
+    path(f'{API_PATH}/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(f'{API_PATH}/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
