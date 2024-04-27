88c2e6a18c50642e7aba50729369a89fea00588d
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 01:42:31 +0300

    UPDATE: user serializer(photo)

--- backend/src/apps/accounts/api/serializers/user_serializers.py
+++ backend/src/apps/accounts/api/serializers/user_serializers.py
@@ -13,4 +13,14 @@ class UserSerializer(serializers.ModelSerializer):
             "about_me",
             "type",
             "date_added",
+            "photo_url"
         ]
+        
+    def get_photo_url(self, obj):
+        if obj.photo and hasattr(obj.photo, "url"):
+            request = self.context.get("request")
+            photo_url = obj.photo.url
+            return request.build_absolute_uri(
+                photo_url
+            )  # Build full URL using the request object
+        return None  
