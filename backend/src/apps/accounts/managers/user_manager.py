9ffe8a7e8b4dc18cc230e5b44a2ecdd2a8f0a88c
Yurii <yuriydorosh2005@gmail.com>
2024-04-27 22:12:04 +0300

    FIX: user_manager(added gettext_lazy)

--- backend/src/apps/accounts/managers/user_manager.py
+++ backend/src/apps/accounts/managers/user_manager.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import BaseUserManager
-
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, password, **extra_fields):
