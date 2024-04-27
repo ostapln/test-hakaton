6c2caa52343d8e61b5821d2fb80e318bb8669ee7
Yurii <yuriydorosh2005@gmail.com>
2024-04-27 23:54:33 +0300

    ADD: serializers/user_serializers.py with UserSerializer

---
+++ backend/src/apps/accounts/api/serializers/user_serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from apps.accounts.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "username",
+            "type",
+            "date_added",
+        ]
