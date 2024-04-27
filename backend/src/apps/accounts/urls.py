93b31c6eab5b726832f6e831efd58787ee02b992
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 00:09:06 +0300

    UPDATE: urls(auth, info, jwt)

--- backend/src/apps/accounts/urls.py
+++ backend/src/apps/accounts/urls.py
@@ -7,3 +7,18 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+
+urlpatterns = [
+    # Auth
+    path("signup/", auth_views.UserRegistrationView.as_view(), name="sign-up"),
+    path("login/", auth_views.UserLoginView.as_view(), name="log-in"),
+    path('change-password/', auth_views.ChangePasswordView.as_view(), name='change-password'),
+    path('delete-account/', auth_views.DeleteUserView.as_view(), name='delete-account'),
+    # User info
+    path('me/', self_views.SelfView.as_view(), name="me"),
+    # JWT
+    path("jwt/create/", auth_views.MyTokenObtainPairView.as_view(), name="jwt-create"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    
+]
