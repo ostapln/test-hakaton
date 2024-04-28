c96d19acfad2c4facb7671b0af15e2ec7bacbec5
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 17:25:26 +0300

    UPDATE: posts admin

--- backend/src/apps/posts/admin.py
+++ backend/src/apps/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.posts.models import Posts
+
+
+@admin.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user_id", "date_added")
+    list_filter = ("id", "user_id", "title")
+    search_fields = ("title", "user_id")
