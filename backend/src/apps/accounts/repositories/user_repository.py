4e374b84e52baea44031de8d448c20739b351c07
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 13:45:34 +0300

    ADD: repositories/user_repository.py

---
+++ backend/src/apps/accounts/repositories/user_repository.py
@@ -0,0 +1,24 @@
+from apps.accounts.models import User
+
+class UserRepository:
+    @staticmethod
+    def user_exists_by_email(email):
+        return User.objects.filter(email=email).exists()
+
+    @staticmethod
+    def user_exists_by_username(username):
+        return User.objects.filter(username=username).exists()
+
+    @staticmethod
+    def create_user(username, email, password, user_type):
+        return User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            type=user_type,
+        )
+
+    @staticmethod
+    def change_user_password(user, new_password):
+        user.set_password(new_password)
+        user.save()
