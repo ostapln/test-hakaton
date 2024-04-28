5d3ba67cd9b5ae4af099746c545993e065e55926
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 15:36:31 +0300

    UPDATE: post serializer(get_photo)

--- backend/src/apps/posts/api/serializers/post_serializers.py
+++ backend/src/apps/posts/api/serializers/post_serializers.py
@@ -5,3 +5,12 @@ class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
         exclude = ['user', ]
+        
+    def get_photo(self, obj):
+        if obj.photo and hasattr(obj.photo, "url"):
+            request = self.context.get("request")
+            photo_url = obj.photo.url
+            return request.build_absolute_uri(
+                photo_url
+            )  # Build full URL using the request object
+        return None  
