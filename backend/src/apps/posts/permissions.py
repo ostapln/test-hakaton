2f21b6170d445eab7300cd8c479ba51c64491eb5
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 15:03:29 +0300

    ADD: permissions for posts

---
+++ backend/src/apps/posts/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
+from apps.posts.models import Posts  
+from apps.posts.repositories.post_repository import PostRepository  
+
+class PostExists(BasePermission):
+    """
+    Allows access only if the post exists.
+    """
+    
+    def has_permission(self, request, view):
+        pk = view.kwargs.get('pk')
+        if PostRepository.post_exists_by_pk(pk=pk):
+            return True
+        else:
+            raise PermissionDenied("This post doesn't exist")
