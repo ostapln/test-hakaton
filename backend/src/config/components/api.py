ae9ce20f3952ffaefc035c3548583317423e38dc
Yurii <yuriydorosh2005@gmail.com>
2024-04-28 12:55:27 +0300

    TEMP: api component(lifetime jwt)

--- backend/src/config/components/api.py
+++ backend/src/config/components/api.py
@@ -26,8 +26,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1440),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
